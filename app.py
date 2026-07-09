e15d8334241970ee8a9dbf4f119e5630c27bbb95
Zhi Chen <zhic33@gmail.com>
2026-07-09 18:56:12 +0800

    add flask task manager interface

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    tasks = [
+        "Finish Flask",
+        "Learn SQL",
+        "Practice LeetCode"
+    ]
+
+    return render_template("index.html", tasks=tasks)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
