fb47717686ac74fbaaae9616fe97015fc0326154
Zhi Chen <zhic33@gmail.com>
2026-07-12 01:55:20 +0800

    remove test code from database.py

--- database.py
+++ database.py
@@ -96,11 +96,3 @@ def delete_task(task_id):
     connection.close()
 
 
-from database import get_all_tasks
-
-tasks = get_all_tasks()
-
-for task in tasks:
-    print(task.title)
-    print(task.deadline)
-    print(task.completed)
